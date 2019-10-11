424526676c21153a8a42b67312ac4255ac019aa4
松尾淳貴 <user@matsuoatsushitakanoMacBook-Pro.local>
2019-10-11 18:13:06 +0900

    20191011

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 /*globals THREE */
 (() => {
-    var dom = document.querySelector('canvas'),
-    width = window.innerWidth,
-    height = window.innerHeight,
-    mp = Math.PI,
-    y = 6,
-    z = 20,
-    reqRet,
-    rad = function(r){return r * (Math.PI / 180);};
+  var dom = document.querySelector('canvas'),
+  width = window.innerWidth,
+  height = window.innerHeight,
+  mp = Math.PI,
+  renderer,
+  ctrl,
+  scene,
+  camera,
+  clock,
+  mixer,
+  y = 6,
+  z = 20,
+  reqRet,
+  rad = function(r){return r * (Math.PI / 180);};
 
 
 
-    (function(w,r){
-        w['r'+r] = w['r'+r] ||
-        w['webkitR'+r] ||
-        w['mozR'+r] ||
-        w['oR'+r] ||
-        w['msR'+r] ||
-        function(callback){w.setTimeout(callback,1000/60);};
-    })(window,'equestAnimationFrame');
+  (function(w,r){
+    w['r'+r] = w['r'+r] ||
+    w['webkitR'+r] ||
+    w['mozR'+r] ||
+    w['oR'+r] ||
+    w['msR'+r] ||
+    function(callback){w.setTimeout(callback,1000/60);};
+  })(window,'equestAnimationFrame');
 
-    (function(w,c){
-        w['c'+c] = w['c'+c] ||
-        w['webkitC'+c] ||
-        w['mozC'+c] ||
-        w['oC'+c] ||
-        w['msC'+c] ||
-        function(callback){w.clearTimeout(callback);};
-    })(window,'ancelAnimationFrame');
+  (function(w,c){
+    w['c'+c] = w['c'+c] ||
+    w['webkitC'+c] ||
+    w['mozC'+c] ||
+    w['oC'+c] ||
+    w['msC'+c] ||
+    function(callback){w.clearTimeout(callback);};
+  })(window,'ancelAnimationFrame');
 
-    var renderer = new THREE.WebGLRenderer({
-        canvas:dom
+  var init = function(){
+    renderer = new THREE.WebGLRenderer({
+      canvas:dom
     });
     renderer.setSize(width,height);
     renderer.setPixelRatio(window.devicePixelRatio);
 
-    var scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-    var camera = new THREE.PerspectiveCamera(60,width / height,1,10000);
+    camera = new THREE.PerspectiveCamera(60,width / height,1,10000);
     camera.position.set(0, y, z);
 
 
@@ -52,7 +59,7 @@
     scene.add(helper);
 
     //OrbitControls
-    var ctrl = new THREE.OrbitControls(camera,dom);
+    ctrl = new THREE.OrbitControls(camera,dom);
     // ctrl.autoRotate = true;
     // ctrl.enableZoom = false;
     // ctrl.enablePan = false;
@@ -60,29 +67,63 @@
     //背景色
     renderer.setClearColor(0x000000, 1.0);
 
+    clock = new THREE.Clock();
+
+
     var cen;
     var loader = new THREE.ColladaLoader();
     loader.load(
       './data/simple.dae',
       function(collada){
+        var anm = collada.animations;
         cen = collada.scene;
+        cen.traverse(function(r){
+          if(r.isSkinnedMesh){
+            r.frustumCulled = false;
+          }
+        });
+
+        mixer = new THREE.AnimationMixer(cen);
+        mixer.clipAction(anm[0]).play();
+
+        console.log(cen.animations);
+
         scene.add(cen);
       });
 
+      // var geo = new THREE.CylinderBufferGeometry(3,3,3);
+      // console.log(geo.attributes.position);
+      // var mat = new THREE.MeshBasicMaterial({color:0xff0000,wireframe:true});
+      // var mesh = new THREE.Mesh(geo,mat);
+      // scene.add(mesh);
+
+      load();
+    };
+    //init fnc
+
     //毎秒描画
     var load = function(){
-        ctrl.update();
-        renderer.render(scene,camera);
-        requestAnimationFrame(load);
-    };
-    load();
+      var delta = clock.getDelta();
+      ctrl.update();
 
+      if(mixer !== undefined){
+        mixer.update(delta);
+      }
+      renderer.render(scene,camera);
+      requestAnimationFrame(load);
+    };
 
     $(window).on('resize',function(){
-        var w = window.innerWidth,
-        h = window.innerHeight;
-        renderer.setSize(w,h);
-        camera.aspect = w / h;
-        camera.updateProjectionMatrix();
+      var w = window.innerWidth,
+      h = window.innerHeight;
+      renderer.setSize(w,h);
+      camera.aspect = w / h;
+      camera.updateProjectionMatrix();
     });
-})();
+
+
+    init();
+
+
+
+  })();
