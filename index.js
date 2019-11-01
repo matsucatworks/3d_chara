0cbe42d6c7bab00e4e6ef15a8604a6f02ebbee4f
松尾淳貴 <user@matsuoatsushitakanoMacBook-Pro.local>
2019-11-01 14:39:05 +0900

    20191101

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-/*globals THREE Stats */
-(() => {
-  var dom = document.querySelector('canvas'),
-  width = window.innerWidth,
-  height = window.innerHeight,
-  mp = Math.PI,
-  renderer,
-  ctrl,
-  scene,
-  camera,
-  clock = new THREE.Clock(),
-  mixer,
-  stats,
-  y = 6,
-  z = 20,
-  reqRet,
-  rad = function(r){return r * (Math.PI / 180);};
-
-
+/* globals THREE Stats */
+// import * as THREE from 'https://threejs.org/build/three.module.js';
+//
+// import Stats from 'https://threejs.org/examples/jsm/libs/stats.module.js';
+//
+// import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+// import { FBXLoader } from 'https://threejs.org/examples/jsm/loaders/FBXLoader.js';
+(function(){
 
   (function(w,r){
     w['r'+r] = w['r'+r] ||
@@ -36,60 +25,87 @@
     function(callback){w.clearTimeout(callback);};
   })(window,'ancelAnimationFrame');
 
-  var init = function(){
-    renderer = new THREE.WebGLRenderer({
-      canvas:dom
-    });
-    renderer.setSize(width,height);
-    renderer.setPixelRatio(window.devicePixelRatio);
+  var dom,
+  stats,
+  mp = Math.PI,
+  ctrl,
+  camera,
+  scene,
+  renderer,
+  light,
+  clock = new THREE.Clock(),
+  rad = function(r){return r * (Math.PI / 180);},
+  mixer;
 
-    stats = new Stats();
-    document.body.appendChild(stats.dom);
+
+  var init = function() {
+
+    dom = document.querySelector('canvas');
+
+    renderer = new THREE.WebGLRenderer({ antialias: true,canvas:dom });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.shadowMap.enabled = true;
 
     scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xa0a0a0 );
+    scene.fog = new THREE.Fog( 0xa0a0a0, 200, 800 );
 
-    camera = new THREE.PerspectiveCamera(60,width / height,1,10000);
-    camera.position.set(0, y, z);
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.set( 200, 200, 300 );
 
+    light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+    light.position.set( 0, 100, 0 );
+    scene.add( light );
 
-    //ライト
-    var light_1 = new THREE.DirectionalLight(0xffffff,1);
-    var light_2 = new THREE.AmbientLight(0xffffff,1);
-    scene.add(light_2);
-    light_1.position.set(0,10,20);
-    // scene.add(light_1);
+    light = new THREE.DirectionalLight( 0xffffff,0.5 );
+    light.position.set( 0, 100, 200 );
+    light.castShadow = true;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = - 100;
+    light.shadow.camera.left = - 100;
+    light.shadow.camera.right = 100;
+    scene.add( light );
 
-    var helper = new THREE.DirectionalLightHelper(light_1);
-    scene.add(helper);
+    // scene.add( new CameraHelper( light.shadow.camera ) );
 
-    //OrbitControls
-    ctrl = new THREE.OrbitControls(camera,dom);
-    // ctrl.autoRotate = true;
-    // ctrl.enableZoom = false;
-    // ctrl.enablePan = false;
+    // ground
+    var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 500, 500 ),
+    new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+    mesh.rotation.x = rad(-90);
+    mesh.receiveShadow = true;
+    scene.add( mesh );
 
-    //背景色
-    renderer.setClearColor(0x000000, 1.0);
+    var grid = new THREE.GridHelper( 1000, 40, 0xff0000, 0x000000 );
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    scene.add( grid );
 
-    // clock = new THREE.Clock();
-
-
-    var cen;
-
+    // model
     var loader = new THREE.ColladaLoader();
+    // var loader = new THREE.FBXLoader();
     loader.load(
-      './data/simple.dae',
-      function(d){
-        cen = d.scene;
-        mixer = new THREE.AnimationMixer(cen);
-        mixer.clipAction(d.animations[0]).play();
+      // 'bone_move.fbx',
+      'data/simple.dae',
+      function ( d ) {
+        mixer = new THREE.AnimationMixer( d.scene );
+        mixer.clipAction( d.animations[ 0 ] ).play();
 
-        cen.rotation.z = rad(-30);
+        d.scene.traverse( function ( child ){
+          if ( child.isMesh ) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+          }
+        });
 
-        scene.add(cen);
+        d.scene.scale.x = 10;
+        d.scene.scale.y = 10;
+        d.scene.scale.z = 10;
+
+        d.scene.position.y = 48;
+        scene.add( d.scene );
       },
-      // called while loading is progressing
-      function ( xhr ) {
+      function(xhr){
         var per =  (xhr.loaded / xhr.total * 100) | 0;
         $('.loading div').html(per + '%');
         $('.loading span span').attr('style','width:'+per+'%;');
@@ -97,33 +113,31 @@
         if(xhr.loaded === xhr.total){
           $('.loading').addClass('opacity');
         }
-
-        console.log(xhr);
-
-      },
-      // called when loading has errors
-      function ( error ) {
-
-        console.log( 'An error happened' );
-
+        // console.log(xhr);
       }
     );
 
-
-      load();
-    };
-    //init fnc
-
-    //毎秒描画
-    var load = function(){
+      ctrl = new THREE.OrbitControls( camera, renderer.domElement );
+      ctrl.target.set( 0, 50, 0 );
+      ctrl.enablePan = true;
       ctrl.update();
 
+      // stats
+      stats = new Stats();
+      document.body.appendChild( stats.dom );
+
+      load();
+
+    };
+    //init
+
+    var load = function() {
       if(mixer !== undefined){
         mixer.update(clock.getDelta());
       }
-      renderer.render(scene,camera);
+      renderer.render( scene, camera );
       stats.update();
-      requestAnimationFrame(load);
+      requestAnimationFrame( load );
     };
 
     $(window).on('resize',function(){
@@ -134,11 +148,7 @@
       camera.updateProjectionMatrix();
     });
 
-
     init();
-
-
-
 
 
   })();
